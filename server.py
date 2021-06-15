29c7c3aa83dda8ade2d920aa603f41373c41cf0d
tevawolf <tevawolf@yahoo.co.jp>
2021-06-15 23:35:26 +0900

    更新

--- server.py
+++ server.py
@@ -4,4 +4,4 @@ from simplebbs.app import app
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
