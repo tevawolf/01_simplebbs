01fd3164ee46dcc4e71ae9b6f224bd2dbcc1353e
tevawolf <tevawolf@yahoo.co.jp>
2021-06-17 22:22:00 +0900

    更新

--- simplebbs/app.py
+++ simplebbs/app.py
@@ -7,8 +7,9 @@ from simplebbs.application.service.InitView import InitView
 
 def create_app():
 
+    app = Flask(__name__)
     # app = Flask(__name__, static_folder="presentation/static")
-    app = Flask(__name__, static_folder="presentation/static", template_folder="presentation/templates")
+    # app = Flask(__name__, static_folder="presentation/static", template_folder="presentation/templates")
     # app.jinja_loader = FileSystemLoader('simplebbs/presentation/templates')
     app.config.from_object('simplebbs.config.Config')
 
