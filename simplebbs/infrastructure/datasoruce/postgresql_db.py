634307f448f9199d610cb44fcafc604ff5b09684
tevawolf <tevawolf@yahoo.co.jp>
2021-06-19 19:50:06 +0900

    更新

--- simplebbs/infrastructure/datasoruce/postgresql_db.py
+++ simplebbs/infrastructure/datasoruce/postgresql_db.py
@@ -3,10 +3,12 @@ import os
 import psycopg2
 from flask import g
 
-DATABASE_HEROKU = 'host=' + str(os.environ.get('DATABASE_URL')) + ' port=5432' + \
-           ' dbname=d4o6thnl90bnmp' + \
-           ' user=vokuyzrqbczpmk' + \
-           ' password=c4f68a9ff954e2cedb70673c5bc22fddf16b67be50b948ee8616acc93413e5dd'
+# DATABASE_HEROKU = 'host=' + str(os.environ.get('DATABASE_URL')) + ' port=5432' + \
+#            ' dbname=d4o6thnl90bnmp' + \
+#            ' user=vokuyzrqbczpmk' + \
+#            ' password=c4f68a9ff954e2cedb70673c5bc22fddf16b67be50b948ee8616acc93413e5dd'
+
+DATABASE_HEROKU = os.environ.get('DATABASE_URL')
 
 DATABASE_LOCAL = 'host=127.0.0.1 port=5432 dbname=warewolf user=tevawolf password=teVa0210'
 
